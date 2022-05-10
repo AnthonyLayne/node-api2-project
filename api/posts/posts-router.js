27161811fdad76f37d2a7c0ace9b464a62942aa4
AnthonyLayne <kepheus.ds@gmail.com>
2022-05-10 18:59:56 -0400

    PUT/DELETE setup for posting new and deleting

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -60,8 +60,62 @@ router.post("/", (req, res) => {
   }
 });
 
-router.delete("/:id", (req, res) => {});
-router.put("/:id", (req, res) => {});
+router.put("/:id", (req, res) => {
+  const { title, contents } = req.body;
+  if (!title || !contents) {
+    res.status(400).json({
+      message: "Please provide title and contents for the post",
+    });
+  } else {
+    POST.findById(req.params.id)
+      .then((post) => {
+        if (!post) {
+          res.status(404).json({
+            message: "The post with the specified ID does not exist",
+          });
+        } else {
+          return POST.update(req.params.id, req.body);
+        }
+      })
+      .then((data) => {
+        if (data) {
+          return POST.findById(req.params.id);
+        }
+      })
+      .then((post) => {
+        if (post) {
+          res.json(post);
+        }
+      })
+      .catch((err) => {
+        res.status(500).json({
+          message: "The post information could not be modified",
+          err: err.message,
+          stack: err.stack,
+        });
+      });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const delPost = await POST.findById(req.params.id);
+    if (!delPost) {
+      res.status(404).json({
+        message: "The post with the specified ID does not exist",
+      });
+    } else {
+      await POST.remove(req.params.id);
+      res.json(delPost);
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "The post could not be removed",
+      err: err.message,
+      stack: err.stack,
+    });
+  }
+});
 router.get("/:id", (req, res) => {});
 
 module.exports = router;
