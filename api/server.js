4e9acecc8608e77a3c0357ce6538f336b7e0d704
AnthonyLayne <kepheus.ds@gmail.com>
2022-05-10 17:35:08 -0400

    setting up post-router

--- api/server.js
+++ api/server.js
@@ -7,6 +7,9 @@ const server = express();
 
 server.use(express.json());
 
+const postRouter = require("./posts/posts-router");
+server.use("/api/posts", postRouter);
+
 server.use("*", (req, res) => {
   res.status(404).json({
     message: "not found",
